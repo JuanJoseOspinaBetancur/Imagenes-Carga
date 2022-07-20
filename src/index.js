1a96bf75fabada6510de732715853c0b09dc5a56
juanjoseospina123321 <juan.jose.ospina@hotmail.com>
2022-07-20 02:15:28 -0500

    imagenes con carga condicional

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {registerImage} from './lazy'
 
-console.log('Happy hacking :)')
+const API = "https://randomfox.ca/floof/";
+const agregarIMG = document.getElementById("agregarIMG");
+
+const arrayImg = [];
+
+const visualizar = (img) => {
+  const images = document.getElementById("images");
+  images.textContent = "";
+  
+  
+    const imagen = document.createElement("img");
+    imagen.dataset.src = img; 
+    imagen.setAttribute("class", "w-96 h-96 mx-auto rounded-lg");
+
+    const cont=document.createElement("div")
+    cont.append(imagen)
+    cont.setAttribute("class","bg-gray-200")
+    
+    arrayImg.push(cont);
+    registerImage(cont)
+  
+
+  images.append(...arrayImg);
+};
+
+const obtener = async (url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    visualizar(data.image)
+  } catch (error) {
+    console.log(error);
+  }
+
+};
+
+agregarIMG.addEventListener("click", () => {
+  obtener(API);
+});
